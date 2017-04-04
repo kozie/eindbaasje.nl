24e2d145b9f238f33973a5fd5b804bf86a7cb256
Koos van Egmond <koos@cream.nl>
2017-04-04 10:43:21 +0200

    Autoprefixer! Gradient background and variables

--- themes/kozie/gulpfile.js
+++ themes/kozie/gulpfile.js
@@ -4,14 +4,20 @@ const gulp = require("gulp");
 
 const babel = require("gulp-babel");
 const sass = require("gulp-sass");
+const autoprefixer = require('gulp-autoprefixer');
 
 const sourcemaps = require("gulp-sourcemaps");
+
+const autoprefixerOptions = {
+    'browsers': ['last 3 versions']
+};
 
 gulp.task("styles", () => {
     return gulp.src('./src/css/styles.scss')
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(sourcemaps.write())
+        .pipe(autoprefixer(autoprefixerOptions))
         .pipe(gulp.dest('./static/css'));
 });
 
